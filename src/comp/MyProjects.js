1a529e56e95a4a4a601dc00830138c118834d784
Victorowsky <Victorowskyy@gmail.com>
2021-03-17 13:26:17 +0100

    <Project/> -> Fylo changed

--- src/comp/MyProjects.js
+++ src/comp/MyProjects.js
@@ -1,58 +1,68 @@
 import React from "react";
 import "./MyProjects.css";
 import { DataContext } from "../App";
-import { useContext,useEffect } from "react";
-import gsap from 'gsap';
+import { useContext, useEffect } from "react";
+import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import Project from "./Project";
 
 const MyProjects = () => {
-  gsap.registerPlugin(ScrollTrigger)
+	gsap.registerPlugin(ScrollTrigger);
 
-  const { myProjectsRef } = useContext(DataContext);
+	const { myProjectsRef } = useContext(DataContext);
 
-  useEffect(()=>{
-    if(window.innerWidth > 500 && window.innerHeight < 1080){
-       const tl = gsap.timeline({
-      scrollTrigger:{
-        trigger: myProjectsRef.current,
-        start: '-85% top',
-      },
-   
-    })
-    const arrayOfProjects = Array.from(myProjectsRef.current.children)
-    tl.from(arrayOfProjects[1].children, {y:'+=300', duration: 1, ease: "power1.out", autoAlpha:0, stagger: 0.25})
-  
-    tl.from(myProjectsRef.current.children[0],{y:'+=100', duration: 1, ease: "power1.out", autoAlpha:0,}, '-=1' )
-    }
-  },[myProjectsRef])
+	useEffect(() => {
+		if (window.innerWidth > 500 && window.innerHeight < 1080) {
+			const tl = gsap.timeline({
+				scrollTrigger: {
+					trigger: myProjectsRef.current,
+					start: "-85% top",
+				},
+			});
+			const arrayOfProjects = Array.from(myProjectsRef.current.children);
+			tl.from(arrayOfProjects[1].children, {
+				y: "+=300",
+				duration: 1,
+				ease: "power1.out",
+				autoAlpha: 0,
+				stagger: 0.25,
+			});
 
-  return (
-    <div className="myProjectsContainer">
-      <div className="myProjectRef" ref={myProjectsRef}>
-        <h2>Some of my projects</h2>
-        <div className="myProjects">
-  
-          <Project link={'https://boiling-bastion-80662.herokuapp.com/'}
-          imgLink={'https://imgur.com/Of1TP3X.png'} projectName={"Watch Together"}
-          description={`Watch Together, connected with Twitch chat. Store data on MongoDB, and using Socket.io to emit events`}
-          />
-          <Project link={'https://victorowsky.github.io/frontmentor-blog/'}
-          imgLink={'https://imgur.com/ZZlUCca.png'} projectName={"Blogr"}
-          description={`Project from Frontend Mentor. Made in React within about 8 hours.`}
-          />
-           <Project link={'https://victorowsky.github.io/StreamingFamily/#/'}
-          imgLink={'https://imgur.com/eebZgAK.png'} projectName={"Streaming Family"}
-          description={`Platform where you can find people to share your account,
-          thoughts, and more.`}
-          />
-               
-        
-  
-        </div>
-      </div>
-    </div>
-  );
+			tl.from(
+				myProjectsRef.current.children[0],
+				{ y: "+=100", duration: 1, ease: "power1.out", autoAlpha: 0 },
+				"-=1"
+			);
+		}
+	}, [myProjectsRef]);
+
+	return (
+		<div className="myProjectsContainer">
+			<div className="myProjectRef" ref={myProjectsRef}>
+				<h2>Some of my projects</h2>
+				<div className="myProjects">
+					<Project
+						link={"https://boiling-bastion-80662.herokuapp.com/"}
+						imgLink={"https://imgur.com/Of1TP3X.png"}
+						projectName={"Watch Together"}
+						description={`Watch Together, connected with Twitch chat. Store data on MongoDB, and using Socket.io to emit events`}
+					/>
+					<Project
+						link={"https://victorowsky.github.io/frontmentor-blog/"}
+						imgLink={"https://imgur.com/ZZlUCca.png"}
+						projectName={"Blogr"}
+						description={`Project from Frontend Mentor. Made in React within about 8 hours.`}
+					/>
+					<Project
+						link={"https://victorowsky.github.io/Fylo"}
+						imgLink={"https://imgur.com/AzMj9az.png"}
+						projectName={"Fylo"}
+						description={`Next project from Frontent Mentor. Show some CSS skils.`}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MyProjects;
